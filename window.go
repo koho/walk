cff812408e0c320f9d5247dd5af1e4c916c038ea
koho <gerhard.gh.ta@gmail.com>
2020-05-25 20:46:17 +0800

    Update window.go

--- window.go
+++ window.go
@@ -484,7 +484,7 @@ func MustRegisterWindowClassWithWndProcPtrAndStyle(className string, wndProcPtr 
 		panic("GetModuleHandle")
 	}
 
-	hIcon := win.LoadIcon(hInst, win.MAKEINTRESOURCE(7)) // rsrc uses 7 for app icon
+	hIcon := win.LoadIcon(hInst, win.MAKEINTRESOURCE(11)) // rsrc uses 7 for app icon
 	if hIcon == 0 {
 		hIcon = win.LoadIcon(0, win.MAKEINTRESOURCE(win.IDI_APPLICATION))
 	}
